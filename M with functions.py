eb47adbdf7c1f47e8f630b0a5e19b0e2c3647af6
Kustimurdi <pfaffenzeller.nikolas@gmail.com>
2020-04-04 12:42:28 +0200

    commentary and order

--- M with functions.py
+++ M with functions.py
@@ -5,41 +5,55 @@ import matplotlib.pyplot as plt
 from random import *
 import math
 
+
+N = 200
 '''length of state'''
-N = 200  # length of state
 time = 10
 W = 1.5
+'''disorder paramter'''
 
-'''creates h3 for a given amount of positions N jo boi'''
+
 def calcH3MatOpenBound(N):
+    '''creates h3 for a given amount of positions "N"'''
     h3Mat = np.zeros((N - 1, N - 1), dtype=np.complex)
-    for k in range(N - 2):  # the off-diagonals
-        h3Mat[k][k + 1] = -1
-        h3Mat[k + 1][k] = -1
-    return h3Mat
 
-def calcH3MatPerBound(N):  # ist unnötig bzw. funktioniert icht
-    h3Mat = np.zeros((N - 1, N - 1), dtype=np.complex)
+    #put ones on the off-diagonals
     for k in range(N - 2):
         h3Mat[k][k + 1] = -1
         h3Mat[k + 1][k] = -1
+
+    return h3Mat
+
+
+def calcH3MatPerBound(N):
+    '''unnecessary function which doesn't work'''
+    h3Mat = np.zeros((N - 1, N - 1), dtype=np.complex)
+
+    for k in range(N - 2):
+        h3Mat[k][k + 1] = -1
+        h3Mat[k + 1][k] = -1
+
     h3Mat[N - 2][0] = -1
     h3Mat[0][N - 2] = -1
     return h3Mat
 
 
 def calcHamTimeEvo(matrix, time):
+    '''creates the time evolution for a given hamiltonian "matrix" and point of "time"'''
     matExp = matrix * complex(0, -1) * time
     matExp = expm(matExp)
     return matExp
 
 
 # creation of dipoles
-def calcDipole(N, position, column):  # column vector for column == true and row vector for column == false
+def calcDipole(N, position, column):
+    '''creates a vector with the lenght "N"-1 with a one at "position", for "column" == True it is a column vector, for "column" == False it is a row vector (dipole at position "position")'''
+
     if column:
         dipoleR = np.zeros((N - 1, 1))
         dipoleR[position - 1][0] = 1
         return dipoleR
+
     else:
         dipoleL = np.zeros((1, N - 1))
         dipoleL[0][position - 1] = 1
@@ -47,6 +61,8 @@ def calcDipole(N, position, column):  # column vector for column == true and row
 
 
 def calcProbDist(matrix):  # erster Versuch der Wahrscheinlichkeitsverteilung. funktioniert nicht so gut
+    '''first try of a probability distribution function which doesn't work so well'''
+
     N = len(matrix) + 1
     diff = list(range(2 - N, N - 1))
     probDis = []
@@ -76,18 +92,28 @@ def calcProbDist(matrix):  # erster Versuch der Wahrscheinlichkeitsverteilung. f
 
 
 # probability distribution
-def calcProbDistMiddle(
-        matrix):  # bestimmt die wahrscheinlichkeitsverteilung für eine Zeitentwicklung des Dipols -> matrix == matrix exponential des Hamiltonian
+def calcProbDistMiddle(matrix):  # bestimmt die wahrscheinlichkeitsverteilung für eine Zeitentwicklung des Dipols -> matrix == matrix exponential des Hamiltonian
+    '''takes the time evolution of a hamiltonian matrix \n
+     returns a tupel of firstly the distance between an initial and a referential dipole and secondly the respecting probability distribution'''
+
     N = len(matrix)
     halfN = math.ceil(N / 2)
+
+    #creates the list of distances between the inital and referential dipole
     if halfN % 2 == 0:
         diff = list(range(-halfN + 1, halfN + 1))
     else:
         diff = list(range(-halfN + 1, halfN))
+
+
     probDis = []
+
+    #creates the time evolution of the inital dipole
     dipoleInit = np.zeros((N, 1))
     dipoleInit[halfN - 1][0] = 1
     dipoleInitTime = matrix.dot(dipoleInit)
+
+    #calculates the overlap of the reference dipoles with the time evolved inital dipole
     dipoleRef = np.zeros((1, N))
     for x in diff:
         dipoleRef[0][halfN - x - 1] = 1
@@ -95,25 +121,33 @@ def calcProbDistMiddle(
         result = skalProd[0][0].real ** 2 + skalProd[0][0].imag ** 2
         probDis.append(result)
         dipoleRef[0][halfN - x - 1] = 0
+
     ergebnis = []
     ergebnis.append(diff)
     ergebnis.append(probDis)
     return ergebnis
 
 
-def plotDist(listWithListsWithValues, xlabel, ylabel):
-    plt.plot(listWithListsWithValues[0], listWithListsWithValues[1], "r")
+def plotDist(tupleOfListsOfValues, xlabel, ylabel):
+    '''takes a tuple of lists with x and y values: "tupleOfListsOfValues'''
+    plt.plot(tupleOfListsOfValues[0], tupleOfListsOfValues[1], "r")
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
 
 
 # adding the disorder
 def calcDisorder(N, disorderParameter):  # kreiert die additionale Matrix der Störung
+    '''takes an amount of positions "N" and a disorder parameter "disorderParameter" \n
+    returns the respecting disorder matrix'''
     W = disorderParameter
+
+    #goes through the positions, appoints to them random values between [-W,W]
     hDis = []
     for i in range(N):
         h = uniform(-W, W)
         hDis.append(h)
+
+    #creates a matrix and places the difference of the values of two adjacent positions on the respecting position of the diagonal of the matrix
     disMat = np.zeros((N - 1, N - 1))
     for i in range(N - 1):
         disMat[i][i] = hDis[i] - hDis[i + 1]
@@ -121,9 +155,13 @@ def calcDisorder(N, disorderParameter):  # kreiert die additionale Matrix der St
 
 
 def calcStandDev(probDis):
+    '''takes the tuple of the calcProbDistMiddle method "probDis" (the probability distribution for a given time evolution of a hamiltonian matrix) \n
+    returns the standard deviation of the distribution'''
     lenthOfState = len(probDis[1])
     valueOne = 0
     valueTwo = 0
+
+    #computes the two values in the square root of the equation for the deviation
     for i in range(lenthOfState):
         valueOne += probDis[1][i] * (i + 1) ** 2
         valueTwo += probDis[1][i] * (i + 1)
@@ -132,16 +170,25 @@ def calcStandDev(probDis):
 
 
 def calcStandDevList(startTime, endTime, incrementTime, matrix):
+    '''takes a start and ending time, the increment for the time and a hamiltonian matrix \n
+    returns a tuple of the time steps and the respecting standard deviations'''
     timeList = range(startTime, endTime + 1, incrementTime)
+
+    #creates a list of time evolved hamiltonian matrices
     matExpList = []
     for i in timeList:
         matExpList.append(calcHamTimeEvo(matrix, i))
+
+    #creates a list of probability distributions for the matrices
     probDisList = []
     for i in matExpList:
         probDisList.append(calcProbDistMiddle(i))
+
+    #creates a list of standard deviations for the probability distributions
     sigmaList = []
     for i in probDisList:
         sigmaList.append(calcStandDev(i))
+
     result = []
     result.append(timeList)
     result.append(sigmaList)
@@ -149,19 +196,30 @@ def calcStandDevList(startTime, endTime, incrementTime, matrix):
 
 
 def calcProbSumList(startTime, endTime, incrementTime, matrix):
+    '''exists to test the crediblity of the probability distribution \n
+    takes a start and ending time, the increment for the time and a hamiltonian matrix \n
+    returns a tuple of the time steps and the sums of the respecting probability distributions \n
+    (the list of the sums should up to a point of time only contain ones, for big enough time values the boundaries of the chain should be reached and values apart from one could appear'''
     timeList = range(startTime, endTime + 1, incrementTime)
+
+    #creates a list of time evolved hamiltonian matrices
     matExpList = []
     for i in timeList:
         matExpList.append(calcHamTimeEvo(matrix, i))
+
+    # creates a list of probability distributions for the matrices
     probDisList = []
     for i in matExpList:
         probDisList.append(calcProbDistMiddle(i))
+
+    #creates a list of the sums of the probability distributions
     probSumList = []
     for i in probDisList:
         k = 0
         for j in i[1]:
             k += j
         probSumList.append(k)
+
     result = []
     result.append(timeList)
     result.append(probSumList)
@@ -176,6 +234,40 @@ h3DisMat = h3Mat + disMat  # kombinierte matrix von hamiltonian und störung
 h3DisExp = calcHamTimeEvo(h3DisMat, time)  # matrixexponential von hamiltonian mit störung
 h3DisProbDist = calcProbDistMiddle(h3DisExp)  # wahrscheinlichkeitsverteilung für h3 mit störung
 
+#plot for probability distribution
+figProbDist = plt.figure()
+ax1ProbDist = figProbDist.add_subplot(2,2,1)
+plotDist(h3ProbDist,"n - m","p(x)") #plot for the
+ax2ProbDist = figProbDist.add_subplot(2,2,2)
+plotDist(h3DisProbDist,"n - m","p(x)")
+plt.show()
+
+
+sigmaH3 = calcStandDevList(0, 30, 1, h3Mat) #list of standard deviations for the h3 matrix
+sigmaH3Dis = calcStandDevList(0, 30, 1, h3DisMat) #list of standard deviations for the h3 matrix with the random disorder
+
+#plot for the time evolution of the standard deviation
+figSigma = plt.figure()
+ax1Sigma = fig.add_subplot(2, 2, 1)
+plotDist(sigmaH3, "time", "sigma")
+ax2Sigma = fig.add_subplot(2, 2, 2)
+plotDist(sigmaH3Dis, "time", "sigma")
+plt.show()
+
+
+sumH3 = calcProbSumList(0, 30, 1, h3Mat)
+sumH3Dis = calcProbSumList(0, 30, 1, h3DisMat)
+
+#plot of the time evolution of the sum of the probability distribution
+figsum = plt.figure()
+ax1Sum = fig.add_subplot(2, 2, 1)
+plotDist(sumH3, "time", "sum")
+ax2Sum = fig.add_subplot(2, 2, 2)
+plotDist(sumH3Dis, "time", "sum")
+plt.show()
+
+
+
 k = 0
 for i in h3ProbDist[1]:
     k += i
@@ -185,31 +277,6 @@ l = 0
 for m in h3DisProbDist[1]:
     l += m
 print(l)  # summe der wahrscheinlichkeiten für h3 mit störung
-
-fig = plt.figure()
-# ax1 = fig.add_subplot(2,2,1)
-# plotDist(h3ProbDist,"n - m","p(x)")
-# ax2 = fig.add_subplot(2,2,2)
-# plotDist(h3DisProbDist,"n - m","p(x)")
-
-
-sigmaH3 = calcStandDevList(0, 30, 1, h3Mat)
-sigmaH3Dis = calcStandDevList(0, 30, 1, h3DisMat)
-
-ax3 = fig.add_subplot(2, 2, 3)
-plotDist(sigmaH3, "time", "sigma")
-ax4 = fig.add_subplot(2, 2, 4)
-plotDist(sigmaH3Dis, "time", "sigma")
-
-sumH3 = calcProbSumList(0, 30, 1, h3Mat)
-sumH3Dis = calcProbSumList(0, 30, 1, h3DisMat)
-
-ax5 = fig.add_subplot(2, 2, 1)
-plotDist(sumH3, "time", "sum")
-ax6 = fig.add_subplot(2, 2, 2)
-plotDist(sumH3Dis, "time", "sum")
-
-plt.show()
 
 # test zum überprüfen, ob die position der dipole eine rolle spielt
 dipoleR3 = calcDipole(N, 3, True)
